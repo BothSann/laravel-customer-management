87d621706e77efebc2d9d0e2dae3e2a7647d4c9c
BothSann <boththann76@gmail.com>
2025-03-02 13:38:14 +0700

    Implement delete functionality for customers in index view

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -55,7 +55,11 @@
                                 <td>
                                     <a href="{{ route("customers.edit", $customer->id) }}" style="color: #2c2c2c;" class="ms-1 me-1"><i class="far fa-edit"></i></a>
                                     <a href="{{ route("customers.show", $customer->id) }}" style="color: #2c2c2c;" class="ms-1 me-1"><i class="far fa-eye"></i></a>
-                                    <a href="" style="color: #2c2c2c;" class="ms-1 me-1"><i class="fas fa-trash-alt"></i></a>
+                                    <a href="javascript:;" onclick="$('.form-{{ $customer->id }}').submit()" style="color: #2c2c2c;" class="ms-1 me-1"><i class="fas fa-trash-alt"></i></a>
+                                    <form action="{{ route("customers.destroy", $customer->id) }}" class="form-{{ $customer->id }}" method="POST" >
+                                        @csrf
+                                        @method("DELETE")
+                                    </form>
                                 </td>
                             </tr>
                         @endforeach
