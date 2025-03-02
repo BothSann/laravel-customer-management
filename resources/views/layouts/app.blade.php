d9f65db3e6feefce4da5491efdd9c5b4e012ea01
BothSann <boththann76@gmail.com>
2025-03-02 13:06:19 +0700

    Add custom stylesheet link to layout for improved styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
     <title>Customers</title>
     <link rel="stylesheet" href="{{ asset("assets/css/bootstrap.css") }}">
     <link rel="stylesheet" href="{{ asset("assets/css/fontawesome.min.css") }}">
+    <link rel="stylesheet" href="{{ asset("assets/css/style.css") }}">
+
 
 </head>
 <body class="overflow-x-hidden">
